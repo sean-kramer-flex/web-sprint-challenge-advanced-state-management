e9895bf24ec2aa1b4b4fcf1b7476c5e26e2c2862
Sean Kramer <seandominickramer@gmail.com>
2021-01-08 20:41:13 -0500

    Create store and Provider in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import reducer from "./reducers";
 
 import "./index.css";
 import App from "./App";
 
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 //Task List:
